0147d7804a96615e42c3035825f9d1c744e15230
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2024-01-17 20:44:26 +0100

    Add simple contact

--- src/lib/components/NavigationItem.svelte
+++ src/lib/components/NavigationItem.svelte
@@ -3,9 +3,9 @@
     export let navTarget: string = "";
 </script>
 
-<button disabled={isActive} data-nav-target={navTarget} on:click class={
+<button data-nav-target={navTarget} on:click class={
     isActive ? 
-        "flex gap-2 items-center text-lg uppercase" : 
+        "transition-all flex gap-2 items-center text-lg uppercase" : 
         "group transition-all flex gap-2 items-center text-lg uppercase clickable opacity-20 "
 }>
     <div class={isActive ? "transition-all w-1.5 h-5 rounded-full bg-white" : "transition-all w-1.5 h-1.5 rounded-full bg-white opacity-0 group-hover:opacity-50"}></div>
--- src/lib/components/views/Contact.svelte
+++ src/lib/components/views/Contact.svelte
@@ -1,1 +1,10 @@
-<span>Contact me</span>
+<script>
+	import Heading from "../typography/Heading.svelte";
+
+</script>
+
+<Heading size={2}>Contact</Heading>
+
+<a href="mailto:bradleyoosterveen@gmail.com" class="clickable inline-block">
+    <button class="px-4 py-1 rounded bg-white text-black">Send an e-mail</button>
+</a>
